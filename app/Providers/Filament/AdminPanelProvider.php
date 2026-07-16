39bc452086cd18d309e0ce75792104a083a6b00e
moriuz123 <putra.lebakk@gmail.com>
2026-07-16 11:23:20 +0700

    style: update Filament admin panel colors to match frontend palette

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,49 @@ class AdminPanelProvider extends PanelProvider
                 'Manajemen Situs',
             ])
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => [
+                    50 => '#e8f0fb',
+                    100 => '#d1e1f7',
+                    200 => '#a3c4f0',
+                    300 => '#75a6e8',
+                    400 => '#4789e1',
+                    500 => '#1e5ca8', // SPBE Blue
+                    600 => '#184a86',
+                    700 => '#123765',
+                    800 => '#0a2463', // SPBE Navy
+                    900 => '#071840',
+                    950 => '#040c20',
+                ],
+                'secondary' => [
+                    50 => '#fffbeb',
+                    100 => '#fef3c7',
+                    200 => '#fde68a',
+                    300 => '#fcd34d',
+                    400 => '#fbbf24',
+                    500 => '#e8a020', // SPBE Gold
+                    600 => '#d99015',
+                    700 => '#b45309',
+                    800 => '#92400e',
+                    900 => '#78350f',
+                    950 => '#451a03',
+                ],
+                'danger' => Color::Rose,
+                'gray' => Color::Slate,
+                'info' => Color::Blue,
+                'success' => Color::Emerald,
+                'warning' => [
+                    50 => '#fffbeb',
+                    100 => '#fef3c7',
+                    200 => '#fde68a',
+                    300 => '#fcd34d',
+                    400 => '#fbbf24',
+                    500 => '#e8a020',
+                    600 => '#d99015',
+                    700 => '#b45309',
+                    800 => '#92400e',
+                    900 => '#78350f',
+                    950 => '#451a03',
+                ],
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
