85d5e1718d8768deacd8c175229de325e537585d
moriuz123 <moriuz123@users.noreply.github.com>
2026-07-23 14:44:04 +0700

    fix: auto clear cache on banner create/update/delete

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use App\Models\Concerns\BelongsToOpd;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Banner extends Model
 {
@@ -24,4 +25,15 @@ class Banner extends Model
         'tampil_di_portal' => 'boolean',
         'gambar' => 'array',
     ];
+
+    protected static function booted()
+    {
+        static::saved(function ($banner) {
+            Cache::flush();
+        });
+
+        static::deleted(function ($banner) {
+            Cache::flush();
+        });
+    }
 }
